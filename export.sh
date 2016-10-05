1756671678698b6f982a2a163504e1e0be99192c
Anders Jansson <anders.jansson@atea.se>
2016-10-05 10:58:00 +0200

    Added import.sh & export.sh

--- export.sh
+++ export.sh
@@ -1,16 +1,27 @@
 #!/bin/sh
 
+export RUBYOPT=W0
 
 ## Prerequisites
 echo "## Installing git"
 rpm --quiet -q git && echo "Already installed" || yum -y install git
+if [ ! -f /var/www/miq/vmdb/lib/tasks/rhconsulting_tags.rake ]; then
+  git clone https://github.com/rhtconsulting/cfme-rhconsulting-scripts.git $HOME/cfme-rhconsulting-scripts
+  cd $HOME/cfme-rhconsulting-scripts
+  make install
+else
+  echo "cfme-rhconsulting-scripts: Already installed"
+fi
+
 
 ## Prepare
 echo
 echo "## Downloading AteaCloud Configuration repository"
 cd $HOME
-git clone https://github.com/anjan03/AteaCloudformsConfiguration.git
+[ ! -d AteaCloudformsConfiguration ] && git clone https://github.com/anjan03/AteaCloudformsConfiguration.git
 cd AteaCloudformsConfiguration
+git pull
+
 
 ## Export CFME Database objects
 for i in `ls -d * |egrep -v "vmdb|port.sh"`
